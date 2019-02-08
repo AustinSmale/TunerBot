8b1e2d6d3b798183aef228486a3ca4dcfca98c62
Austin <austin36@msn.com>
2019-02-07 20:35:08 -0500

    now grabs all online users

--- src/main/java/database/DatabaseManager.java
+++ src/main/java/database/DatabaseManager.java
@@ -1,11 +1,16 @@
 package database;
 
+import java.util.List;
 import java.util.ArrayList;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 import org.bson.Document;
+import org.javacord.api.DiscordApi;
+import org.javacord.api.entity.user.User;
+import org.javacord.api.entity.user.UserStatus;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
@@ -15,12 +20,17 @@ public class DatabaseManager {
 	private static DatabaseManager manager = null;
 	private ScheduledExecutorService buttonExecutorService;
 	private MongoCollection<Document> coll;
+	private DiscordApi api;
 
-	private DatabaseManager(String URI) {
+	private DatabaseManager(String URI, DiscordApi api) {
 		if (manager == null) {
 			// connect to database and insert
 			MongoClient mongo = MongoClients.create(URI);
 			coll = mongo.getDatabase("tuner-bot").getCollection("buttons");
+
+			// give manager the api to find users
+			this.api = api;
+
 			// start thread to give buttons every 30 mins
 			startThread();
 			
@@ -34,6 +44,9 @@ public class DatabaseManager {
 	// Task to give buttons
 	Runnable giveButtons = () -> {
 		// find online users that have role verified
+		List<User> onlineUsers = api.getServerById(91082346962358272L).get().getRolesByName("Verified").get(0)
+				.getUsers().stream().filter(u -> u.getDesktopStatus() != UserStatus.OFFLINE)
+				.collect(Collectors.toList());
 		// give 1 button to each
 		// for debug
 		System.out.println("Gave buttons!");
@@ -41,11 +54,11 @@ public class DatabaseManager {
 
 	private void startThread() {
 		buttonExecutorService = Executors.newSingleThreadScheduledExecutor();
-		buttonExecutorService.scheduleAtFixedRate(giveButtons, 30, 30, TimeUnit.MINUTES);
+		buttonExecutorService.scheduleAtFixedRate(giveButtons, 00, 30, TimeUnit.MINUTES);
 	}
 
-	public static DatabaseManager getInstance(String URI) {
-		return new DatabaseManager(URI);
+	public static DatabaseManager getInstance(String URI, DiscordApi api) {
+		return new DatabaseManager(URI, api);
 	}
 
 	public static DatabaseManager getInstace() {
