2350eda6f2ce48cd25d8b913d278e2fcfacca1d6
Austin <austin36@msn.com>
2019-02-08 11:03:58 -0500

    mlab was just being strange, everyone was found and added/updated

--- src/main/java/database/DatabaseManager.java
+++ src/main/java/database/DatabaseManager.java
@@ -12,6 +12,7 @@ import org.javacord.api.DiscordApi;
 import org.javacord.api.entity.user.User;
 import org.javacord.api.entity.user.UserStatus;
 
+import com.google.common.collect.Iterables;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
@@ -21,6 +22,7 @@ public class DatabaseManager {
 	private ScheduledExecutorService buttonExecutorService;
 	private MongoCollection<Document> coll;
 	private DiscordApi api;
+	private boolean writeLock;
 
 	private DatabaseManager(String URI, DiscordApi api) {
 		if (manager == null) {
@@ -30,7 +32,7 @@ public class DatabaseManager {
 
 			// give manager the api to find users
 			this.api = api;
-			
+
 			// start thread to give buttons every 30 mins
 			startThread();
 
@@ -41,16 +43,19 @@ public class DatabaseManager {
 	// Task to give buttons
 	Runnable giveButtons = () -> {
 		// find online users that have role verified
-		List<User> onlineUsers = api.getServerById(91082346962358272L).get().getRolesByName("Verified").get(0)
+		List<User> onlineUsersList = api.getServerById(91082346962358272L).get().getRolesByNameIgnoreCase("Verified").get(0)
 				.getUsers().stream().filter(u -> u.getDesktopStatus() != UserStatus.OFFLINE)
 				.collect(Collectors.toList());
 		// give 1 button to each
-		onlineUsers.forEach(u -> addButton(u, 1));
+		writeLock = true;
+		onlineUsersList.forEach(u -> addButton(u, 1));
+		writeLock = false;
+		
 	};
 
 	private void startThread() {
 		buttonExecutorService = Executors.newSingleThreadScheduledExecutor();
-		buttonExecutorService.scheduleAtFixedRate(giveButtons, 5, 30, TimeUnit.MINUTES);
+		buttonExecutorService.scheduleAtFixedRate(giveButtons, 5, 60, TimeUnit.SECONDS);
 	}
 
 	public static DatabaseManager getInstance(String URI, DiscordApi api) {
@@ -67,10 +72,9 @@ public class DatabaseManager {
 
 	private void addButton(User u, int amount) {
 		// check if user in database
-		Document query = coll.findOneAndDelete(new Document("_id", u.getId()));
+		Document query = coll.findOneAndDelete(new Document("d_id", u.getId()));
 		// if they are
-		if(query != null) 
-		{
+		if (query != null) {
 			Document dbUser = query;
 			int currentButtons = (int) dbUser.get("buttons") + 1;
 			dbUser.replace("buttons", currentButtons);
@@ -82,7 +86,7 @@ public class DatabaseManager {
 	}
 
 	private void addNewUser(Long id) {
-		Document newUser = new Document("_id", id).append("buttons", 1).append("rapsheet",
+		Document newUser = new Document("d_id", id).append("buttons", 1).append("rapsheet",
 				new Document("warings", new ArrayList<String>()).append("kicks", new ArrayList<String>()).append("bans",
 						new ArrayList<String>()));
 		coll.insertOne(newUser);
